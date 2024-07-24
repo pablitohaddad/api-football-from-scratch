2dc0cc943f57cd145bec85f3ab10b5d313cb79d7
pablitohaddad <pablohaddad73@gmail.com>
2024-07-24 12:44:22 -0400

    first commit: add the gitignore, properties of bd MySql and the folder with tests

---
+++ src/main/java/io/github/pablitohaddad/footballfromscratch/FootbalFromScratch.java
@@ -0,0 +1,13 @@
+package io.github.pablitohaddad.footballfromscratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootbalFromScratch {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootbalFromScratch.class, args);
+    }
+
+}
