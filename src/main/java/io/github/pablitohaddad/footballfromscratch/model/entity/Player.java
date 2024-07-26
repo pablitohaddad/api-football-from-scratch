0bb2b74bc649b20d680249fac02ef7bc0b01f4b3
pablitohaddad <pablohaddad73@gmail.com>
2024-07-26 00:25:02 -0400

    feat: i created the entity Team and Player and your dtos, with variables necesaris and bussiness rules.

--- src/main/java/io/github/pablitohaddad/footballfromscratch/model/entity/Player.java
+++ src/main/java/io/github/pablitohaddad/footballfromscratch/model/entity/Player.java
@@ -1,4 +1,40 @@
 package io.github.pablitohaddad.footballfromscratch.model.entity;
 
+import io.github.pablitohaddad.footballfromscratch.model.entity.enums.Position;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "player")
 public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "uuid", updatable = false, nullable = false)
+    private String uuid;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING) // o banco de dados vai salvar uma string da posição
+    @Column(name = "position", nullable = false)
+    private Position position;
+
+    @Column(name = "nationality", length = 20)
+    private String nationality;
 }
